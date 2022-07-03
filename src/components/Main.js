96c623e3d860195013585471105a483a10dfbcb8
Ava <avamsteimle@gmail.com>
2022-07-03 08:54:40 -0700

    carousal

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,59 @@
+import Carousel from 'react-bootstrap/Carousel';
+import img1 from '../images/img1.jpg';
+import img2 from '../images/img2.jpg';
+import img3 from '../images/img3.jpg';
+import img4 from '../images/img4.jpg';
+import img5 from '../images/img5.jpg';
+
 const Main = () => {
     return(
         <div>
+      <Carousel>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={img3}
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      <h3>First slide label</h3>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={img2}
+      alt="Second slide"
+    />
 
-        </div>
+    <Carousel.Caption>
+      <h3>Second slide label</h3>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={img1}
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Third slide label</h3>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={img4}
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Third slide label</h3>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
+</div>
     );
 }
 export default Main;
